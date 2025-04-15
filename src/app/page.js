2e8552e1ee23f0866848a4ece2ff018fba696fdf
aatmk-panse <aatmik.23bcs10082@ms.sst.scaler.com>
2025-04-15 14:04:37 +0530

    Fast

--- src/app/page.js
+++ src/app/page.js
@@ -61,7 +61,7 @@ function VideoSection() {
       <section className="py-24 relative overflow-hidden bg-neutral-100">
         <div className="absolute inset-0 pointer-events-none">
           <DotPattern
-            glow={true}
+            // glow={true}
             className={cn(
               "[mask-image:radial-gradient(800px_circle_at_center,white,transparent)]",
               "opacity-40"
@@ -143,29 +143,6 @@ function FinalCta() {
       <div className="absolute inset-0 overflow-hidden">
         <div className="absolute w-full h-full bg-[radial-gradient(circle_at_center,rgba(59,130,246,0.08)_0%,transparent_70%)]" />
         <div className="absolute bottom-0 left-0 right-0 h-40 bg-gradient-to-t from-white to-transparent" />
-
-        {/* Subtle animated waves */}
-        <motion.div
-          className="absolute inset-0 opacity-20"
-          style={{
-            backgroundImage:
-              "url(\"data:image/svg+xml,%3Csvg width='100' height='20' viewBox='0 0 100 20' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M21.184 20c.357-.13.72-.264 1.088-.402l1.768-.661C33.64 15.347 39.647 14 50 14c10.271 0 15.362 1.222 24.629 4.928.955.383 1.869.74 2.75 1.072h6.225c-2.51-.73-5.139-1.691-8.233-2.928C65.888 13.278 60.562 12 50 12c-10.626 0-16.855 1.397-26.66 5.063l-1.767.662c-2.475.923-4.66 1.674-6.724 2.275h6.335zm0-20C13.258 2.892 8.077 4 0 4V2c5.744 0 9.951-.574 14.85-2h6.334zM77.38 0C85.239 2.966 90.502 4 100 4V2c-6.842 0-11.386-.542-16.396-2h-6.225zM0 14c8.44 0 13.718-1.21 22.272-4.402l1.768-.661C33.64 5.347 39.647 4 50 4c10.271 0 15.362 1.222 24.629 4.928C84.112 12.722 89.438 14 100 14v-2c-10.271 0-15.362-1.222-24.629-4.928C65.888 3.278 60.562 2 50 2c-10.626 0-16.855 1.397-26.66 5.063l-1.767.662C13.223 10.84 8.163 12 0 12v2z' fill='%233B82F6' fill-opacity='0.1' fill-rule='evenodd'/%3E%3C/svg%3E\")",
-            animationName: "wave",
-            animationDuration: "25s",
-            animationTimingFunction: "linear",
-            animationIterationCount: "infinite",
-          }}
-        />
-        <style jsx global>{`
-          @keyframes wave {
-            0% {
-              background-position: 0% 0;
-            }
-            100% {
-              background-position: 100% 0;
-            }
-          }
-        `}</style>
       </div>
 
       <Container>
