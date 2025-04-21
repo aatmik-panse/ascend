c1b0672f0a9bc10d6aec75c98dbf355c478bd84e
aatmk-panse <aatmik.23bcs10082@ms.sst.scaler.com>
2025-04-21 15:33:18 +0530

    new2

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,13 +1,10 @@
 const config = {
-    appName: "Hippo",
-    appDescription:
-      "Hippo is a platform for .",
-    domainName:
-      process.env.NODE_ENV === "development"
-        ? "http://localhost:3000"
-        : "https://hippo.com",
-  };
-  
-  
-  export default config;
-  
+  appName: "Hippo",
+  appDescription: "Hippo is a platform for .",
+  domainName:
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:3000"
+      : "https://certcy.space",
+};
+
+export default config;
